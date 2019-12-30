d43f69bde8dd5d08c5c34738af3862b1b8d641b2
krish3402 <40502996+krish3402@users.noreply.github.com>
2019-12-30 15:19:00 +0530

    Update run-all.sh

--- run-all.sh
+++ run-all.sh
@@ -1,2 +1,1 @@
-cd $JBOSS_HOME
 ./bin/standalone.sh -Djboss.node.name=node2 -Djboss.server.base.dir=$JBOSS_HOME/node2 -Dorg.jboss.boot.log.file=$JBOSS_HOME/node2/configuration/logging.properties -b 0.0.0.0 -bmanagement 0.0.0.0 --server-config=standalone.xml -Djboss.socket.binding.port-offset=2 &
